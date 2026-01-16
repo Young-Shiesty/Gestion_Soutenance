3bb6a0dfa7095c65d695ff4e94ecd47c07bf785c
Youngshiesty <mamzf18@gmail.com>
2026-01-16 01:49:33 +0100

    Mise a jours par rapport au cours  les Loggs sont fait et  toutes les pages sont a jours

---
+++ AppSenSoutenance/Models/Td_Erreur.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSenSoutenance.Models
+{
+    public class Td_Erreur
+    {
+        [Key]
+        public int IdTd_Erreur {  get; set; }
+        [MaxLength(200)]
+        public string TitreErreur { get; set; }
+        [MaxLength(2000)]
+        public string DescriptionErreur { get; set; }
+        public DateTime DateErreur { get; set; }
+
+    }
+}
--- AppSenSoutenance/Program.cs
+++ AppSenSoutenance/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using AppSenSoutenance.Migrations;
+using AppSenSoutenance.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,6 +20,25 @@ namespace AppSenSoutenance
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmConnexion());
+        }
+        public static void StartApp()
+        {
+            BdSenSoutenanceContext db = new BdSenSoutenanceContext();
+            var adminUser = db.admins.Count();
+            if (adminUser == 0)
+            {
+                Admin candidat = new Admin();
+                candidat.NomUtilisateur = "Thiam";
+                candidat.PrenomUtilisateur = "Moussa";
+                candidat.TelUtilisateur = "774536423";
+                candidat.EmailUtilisateur = "admin@yopmail.com";
+                using (MD5 md5Hash = MD5.Create())
+                {
+                    candidat.MotDePasse = Shered.Crypted.GetMd5Hash(md5Hash, "passer123");
+                }
+                db.admins.Add(candidat);
+                db.SaveChanges();
+            }
         }
     }
 }
